e37ab36453bfffb534095b46ffa98fafdf306beb
nsordk <nicolai@nsor.dk>
2012-08-05 10:36:32 +0200

    Changed PlayerChatEvent to AsyncPlayerChatEvent, as old was deprecated

--- src/info/plugmania/mazemania/listeners/PlayerListener.java
+++ src/info/plugmania/mazemania/listeners/PlayerListener.java
@@ -46,7 +46,7 @@ import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -422,7 +422,7 @@ public class PlayerListener implements Listener {
 	}
 	
 	@EventHandler
-	public void onPlayerChat(PlayerChatEvent event) {
+	public void onPlayerChat(AsyncPlayerChatEvent event) {
 		if(!plugin.arena.playing.contains(event.getPlayer())) return;
 		if(!plugin.mainConf.getBoolean("useSeparatePlayerChat", false)) return;
 		event.setCancelled(true);
